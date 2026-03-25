d675e60b928e091ea08dd64efde8f175ac60c13a
isierra1 <isierra@uami.ai>
2026-03-25 16:27:47 -0300

    always load in monochrome mode

--- colors/monochrome.lua
+++ colors/monochrome.lua
@@ -1,2 +1,9 @@
 package.loaded["rose-pine.palette"] = nil
+package.loaded["rose-pine.config"] = nil
+require("rose-pine").setup({
+	monochrome = true,
+	monochrome_black = "#1a1a1a",
+	monochrome_white = "#e8e8e8",
+})
 require("rose-pine").colorscheme()
+
